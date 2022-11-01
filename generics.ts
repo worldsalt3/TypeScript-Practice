f5dc23d450e2a88d40924e0019df2a46849702fd
Worldsalt3 <riliwanhazzan@gmail.com>
2022-11-01 03:49:18 +0100

    learnt more about generic in typescript

--- generics.ts
+++ generics.ts
@@ -1,27 +1,74 @@
 class ArrayOfNumbers {
-    constructor(public collection: number[]) {
+  constructor(public collection: number[]) {}
 
-    }
-
-    get(index: number): number {
-        return this.collection[index]
-    }
+  get(index: number): number {
+    return this.collection[index]
+  }
 }
 
 class ArrayOfStrings {
-    constructor(public collection: string[]) {}
+  constructor(public collection: string[]) {}
 
-    get(index: number): string {
-        return this.collection[index]
-    }
+  get(index: number): string {
+    return this.collection[index]
+  }
 }
 
 class ArrayOfAnything<T> {
-    constructor(public collection: T[]) {}
+  constructor(public collection: T[]) {}
 
-    get(index: number): T {
-        return this.collection[index]
-    }
- }
+  get(index: number): T {
+    return this.collection[index]
+  }
+}
 
- new ArrayOfAnything<string>(['a', 'b', 'c', 'd'])
+const arr = new ArrayOfAnything<string>(['a', 'b', 'c', 'd'])
+
+//  Generics with functions
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+  }
+}
+
+// using generics
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+  }
+}
+
+printAnything<number>([1, 2, 4, 5])
+
+// Generic constraints
+
+class Car {
+  print() {
+    console.log('I am a car')
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a house')
+  }
+}
+
+interface Printable {
+  print(): void
+}
+
+function Print<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print()
+  }
+}
+
+Print<House>([new House(), new House(), new House()])
+Print<Car>([new Car(), new Car(), new Car()])
